9d0f4a9aea451894e836513982987ef6e129e85d
Efen Stakes <efenstakes101@gmail.com>
2020-01-06 20:52:24 +0300

    add app configuration file

---
+++ config.py
@@ -0,0 +1,12 @@
+## imports
+import os
+
+
+class Config(object):
+    TESTING = os.environ.get('TESTING')
+    DEBUG = os.environ.get('DEBUG')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
+    FLASK_ENV = os.environ.get('FLASK_ENV')
+    ## so reqparser returns all errors rather than the first
+    BUNDLE_ERRORS = os.environ.get('BUNDLE_ERRORS')
