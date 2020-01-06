f4ba4da1827997aae7db97633c055ac17c601561
Efen Stakes <efenstakes101@gmail.com>
2020-01-06 20:52:05 +0300

    add app bootstrap file

---
+++ app/__init__.py
@@ -0,0 +1,32 @@
+## imports
+from flask import Flask
+from flask_marshmallow import Marshmallow
+
+
+## create flask object
+app = Flask(__name__, instance_relative_config=True)
+## set app configuration
+app.config.from_object('config.Config')
+
+## create a marshmallow object
+ma = Marshmallow(app)
+
+
+## import module blueprints
+from .contacts import contacts_bp 
+
+
+## add the module blueprints to the app instance
+app.register_blueprint(contacts_bp, url_prefix='/api/contact')
+
+## create more routes
+
+# index
+@app.route('/')
+def index():
+    return { 'page': 'index', 'live': True }
+
+# about
+@app.route('/about')
+def about():
+    return { 'page': 'about', 'live': True }
