a7e55b9c501ec38d2a28f2f69f7525e59e9916b8
Efen Stakes <efenstakes101@gmail.com>
2020-01-08 20:13:27 +0300

    add contact module models with sqlalchemy

--- app/contacts/models.py
+++ app/contacts/models.py
@@ -1,12 +1,21 @@
 ## imports
+from datetime import datetime
+
+from app import db
 
 
-class Contact(object):
-    name = ''
-    email = ''
-    phone = ''
-    added_on = ''
+
+class Contact(db.Model):
+    id = db.Column( db.Integer, unique=True, primary_key=True )
+    account = db.Column( db.String(80), unique=False, nullable=False )
+    name = db.Column( db.String(80), unique=True, nullable=False )
+    email = db.Column( db.String(80), unique=True, nullable=False )
+    phone = db.Column( db.String(12), unique=True, nullable=False )
+    added_on = db.Column( db.DateTime, default=datetime.utcnow )
+
+    ## set table name
+    __tablename__ = 'contacts'
 
     def __repr__(self):
-        return self.name
+        return 'account >> {0}'.format(self.name)
 
