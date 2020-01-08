77549541f76cf1a509782111d7a590a95f3c89d9
Efen Stakes <efenstakes101@gmail.com>
2020-01-08 20:31:33 +0300

    add a way to create app tables

--- app/__init__.py
+++ app/__init__.py
@@ -2,18 +2,20 @@
 from flask import Flask
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy 
+from flask_cors import CORS
 
 
 ## create flask object
 app = Flask(__name__, instance_relative_config=True)
 ## set app configuration
 app.config.from_object('config.Config')
+## allow cross origin requests
+CORS(app)
 
 ## create an sqlalchemy object
 db = SQLAlchemy(app)
 ## create a marshmallow object
 ma = Marshmallow(app)
-
 
 
 ## import module blueprints
@@ -23,7 +25,18 @@ from .contacts import contacts_bp
 ## add the module blueprints to the app instance
 app.register_blueprint(contacts_bp, url_prefix='/api/contact')
 
-## create more routes
+
+
+
+''' before the server serves any request '''
+@app.before_first_request
+def before_any_request():
+    with app.app_context():
+        db.create_all()
+    print('starting server...')
+
+
+### create more routes
 
 # index
 @app.route('/')
